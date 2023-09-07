aab491e2fa011f91c68008d2c69db8fdde32560e
Illei Adrian <illei.adrian@yahoo.com>
2023-09-07 17:14:48 +0200

    Add project files.

---
+++ lab3ex3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab3ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            isPerfectSquare(n);
+        }
+
+        static void isPerfectSquare(double x)
+        {
+            Double result = Math.Sqrt(x);
+            System.Console.WriteLine("Sqrt"+x+":" + result);
+            if (result % 1 == 0)
+            {
+                Console.WriteLine("patrat Perfect");
+            }
+            else
+            {
+                Console.WriteLine("nu este patrat perfect");
+            }
+           
+        }
+    }
+}
